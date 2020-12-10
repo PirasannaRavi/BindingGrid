855e12f976a9926b9121bb5549b804633c74ea34
Pirasanna Ravi <pirasanna.ravi@live.com>
2020-12-10 13:48:35 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace BindingGrid.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            //var mvcName = typeof(Controller).Assembly.GetName();
+            //var isMono = Type.GetType("Mono.Runtime") != null;
+
+            //ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+            //ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+            List<BindingGrid.Models.Value> model = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://wbwcfe.worldbank.org/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                //GET Method  
+
+                HttpResponseMessage response = await client.GetAsync("indc/odata/Viw_A_InitialLoading");
+                if (response.IsSuccessStatusCode)
+                {
+                    var countryDetail = await response.Content.ReadAsStringAsync();
+                    model = JsonConvert.DeserializeObject<BindingGrid.Models.OData>(countryDetail).Value;
+                }
+                else
+                {
+                    Console.WriteLine("Internal server Error");
+                }
+            }
+
+            return View(model);
+        }
+    }
+
+
+}
---
+++ Models/BindGrid.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using Newtonsoft.Json;
+
+namespace BindingGrid.Models
+{
+    public class BindGrid
+    {
+        
+    }
+
+    public class Value
+    {
+        [JsonProperty("COUNTRYNAME")]
+        string COUNTRYNAME;
+        [JsonProperty("REGIONNAME")]
+        string REGIONNAME;
+        [JsonProperty("SECTOR")]
+        string SECTOR;
+        [JsonProperty("SUBSECTOR")]
+        string SUBSECTOR;
+        [JsonProperty("QUESTIONMAPPINGID")]
+        int QUESTIONMAPPINGID;
+    }
+
+    public class OData
+    {
+        [JsonProperty("odata.metadata")]
+        public string Metadata { get; set; }
+        public List<Value> Value { get; set; }
+    }
+}
